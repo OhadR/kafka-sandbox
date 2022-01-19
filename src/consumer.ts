83a84ad2cdc9ec567186bf64826cd4e8b082dda7
OhadR <ohadr.developer@gmail.com>
2022-01-19 13:15:01 +0200

    refactor consumer

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,27 +1,60 @@
 import { EachMessagePayload, Kafka, KafkaMessage } from 'kafkajs';
 
-async function consumer() {
-    const kafka = new Kafka({
-        clientId: 'my-app',
-        brokers: ['localhost:9092', 'kafka2:9092']
-    });
+function debug(msg: string) { console.log(msg); }
 
-    const consumer = kafka.consumer({ groupId: 'test-group' })
+export class KafkaComsumer {
 
-    await consumer.connect()
-    console.log('connected');
+    private static _instance: KafkaComsumer;
+    protected consumer: any;
 
-    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+    protected constructor() {
 
-    await consumer.run({
-        eachMessage: handleMessage,
-    });
+        const kafka = new Kafka({
+            clientId: 'my-app',
+            brokers: ['localhost:9092', 'kafka2:9092']
+        });
+
+        this.consumer = kafka.consumer({ groupId: 'test-group' });
+    }
+
+    public static get instance() {
+        if (!KafkaComsumer._instance)
+            KafkaComsumer._instance = new KafkaComsumer();
+
+        return KafkaComsumer._instance;
+    }
+
+    public async connect() {
+
+        await this.consumer.connect()
+        debug('connected');
+    }
+
+    public async disconnect() {
+
+        await this.consumer.disconnect()
+        debug('disconnected');
+    }
+
+    public async listen() {
+        await this.consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+        await this.consumer.run({
+            eachMessage: handleMessage,
+        });
+    }
 }
 
 async function handleMessage(eachMessagePayload: EachMessagePayload) {
     console.log({
         value: eachMessagePayload.message.value.toString(),
     })
+}
+
+
+async function consumer() {
+    await KafkaComsumer.instance.connect();
+    await KafkaComsumer.instance.listen();
 }
 
 consumer();
